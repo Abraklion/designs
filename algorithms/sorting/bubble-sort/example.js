8acaf604b5bc3d5ee7aede14b95cf7d70212d422
Abraklion <abraklion@gmail.com>
2023-03-17 18:21:26 +0500

    Сортировка вставкой
    Пример:
            new file:   algorithms/sorting/insertion-sort/example.js
    
    Описание:
            new file:   algorithms/sorting/insertion-sort/README.md
            new file:   algorithms/sorting/insertion-sort/img.gif
            new file:   algorithms/sorting/insertion-sort/img2.gif

--- algorithms/sorting/bubble-sort/example.js
+++ algorithms/sorting/bubble-sort/example.js
@@ -22,7 +22,7 @@ function bubbleSort(array) {
     for (let j = 0; j < array.length - 1 - i; j++) {
       // если элемент слева больше элемента справа...
       if (array[j] > array[j + 1]) {
-        /// ... меняем их местами
+        // ... меняем их местами
         [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
       count++
